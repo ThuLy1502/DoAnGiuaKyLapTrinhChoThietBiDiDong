0a6aacd6389c5ab53125529d83ae66ed0d786906
acer <nhahc0110@gmail.com>
2021-12-22 18:24:04 +0700

    Thêm code cho lấy sản phẩm từ từ database vào Thêm giao diện AddProductActivity.java Lỗi không hiển thị sản phẩm

---
+++ app/src/main/java/vn/edu/stu/doangiuakylaptrinhchothietbididong/AddProductActivity.java
@@ -0,0 +1,200 @@
+package vn.edu.stu.doangiuakylaptrinhchothietbididong;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import vn.edu.stu.doangiuakylaptrinhchothietbididong.model.SanPham;
+
+public class AddProductActivity extends AppCompatActivity {
+    EditText edtTen, edtGia, edtKichThuoc, edtPhanLoai;
+    ImageView imgHinh;
+    Button btnLuu, btnThemHinh, btnTroVe;
+    final int REQUEST_CODE_GALLERY = 999;
+    final String DB_PATH_SUFFIX = "/databases/";
+    final String DB_NAME = "dbsanpham.sqlite";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnThemHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        AddProductActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
+
+        btnLuu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ten = edtTen.getText().toString();
+                String gia = edtGia.getText().toString();
+                String kichthuoc = edtKichThuoc.getText().toString();
+                String phanloai = edtPhanLoai.getText().toString();
+
+                SanPham sp = new SanPham(1, ten, phanloai, imageViewToByte(imgHinh),gia, kichthuoc);
+                ghiDuLieu(sp);
+                Toast.makeText(AddProductActivity.this, getString(R.string.toast_product_add), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnTroVe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        AddProductActivity.this,
+                        HomePageActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void ghiDuLieu(SanPham sp) {
+        SQLiteDatabase database = openOrCreateDatabase(
+                DB_NAME,
+                MODE_PRIVATE,
+                null
+        );
+        ContentValues row = new ContentValues();
+        row.put("ten", sp.getTensp());
+        row.put("kichthuoc", sp.getKichthuoc());
+        row.put("gia", sp.getGia());
+        row.put("phanloai", sp.getPhanloai());
+        row.put("hinh", sp.getHinhanh());
+        long insertedID = database.insert(
+                "sanpham",
+                null,
+                row
+        );
+        database.close();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_GALLERY){
+            Uri uri = data.getData();
+            try{
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imgHinh.setImageBitmap(bitmap);
+            }catch (FileNotFoundException ex){
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Intent intent =  new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            } else{
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_permission), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.productlist:
+                Intent intent = new Intent(
+                        AddProductActivity.this,
+                        HomePageActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.brandlist:
+                Intent intent1 = new Intent(
+                        AddProductActivity.this,
+                        BrandActivity.class
+                );
+                startActivity(intent1);
+                break;
+            case R.id.logout:
+                Intent intent2 = new Intent(
+                        AddProductActivity.this,
+                        MainActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.about:
+                Intent intent3 = new Intent(
+                        AddProductActivity.this,
+                        AboutActivity.class
+                );
+                startActivity(intent3);
+                break;
+            case R.id.exit:
+                finishAffinity();
+                System.exit(0);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addControls() {
+        edtTen = findViewById(R.id.edtTen);
+        edtGia = findViewById(R.id.edtGia);
+        edtKichThuoc = findViewById(R.id.edtKichThuocSP);
+        edtPhanLoai = findViewById(R.id.edtPhanLoai);
+        imgHinh = findViewById(R.id.imgHinh);
+        btnThemHinh = findViewById(R.id.btnThemHinh);
+        btnLuu = findViewById(R.id.btnLuuSP);
+        btnTroVe = findViewById(R.id.btnTroVe);
+    }
+}
--- app/src/main/java/vn/edu/stu/doangiuakylaptrinhchothietbididong/HomePageActivity.java
+++ app/src/main/java/vn/edu/stu/doangiuakylaptrinhchothietbididong/HomePageActivity.java
@@ -1,22 +1,198 @@
 package vn.edu.stu.doangiuakylaptrinhchothietbididong;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import vn.edu.stu.doangiuakylaptrinhchothietbididong.adapter.lvAdapter;
+import vn.edu.stu.doangiuakylaptrinhchothietbididong.model.SanPham;
+import vn.edu.stu.doangiuakylaptrinhchothietbididong.model.ThuongHieu;
 
 public class HomePageActivity extends AppCompatActivity {
+    ListView lvSanPham;
+    ArrayList<SanPham> ds;
+    lvAdapter lvSanPhamAdapter;
+    FloatingActionButton fabThem;
+
+    final String DB_PATH_SUFFIX = "/databases/";
+    final String DB_NAME = "dbsanpham.sqlite";
+    ThuongHieu th;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        copyDBFromAsssets();
+        addControls();
+        addEvents();
+        docDsSpTuDb();
+        Intent intent = getIntent();
+        th = (ThuongHieu) intent.getSerializableExtra("hienThiSpTheoTh");
+        docDsSpTheoTh();
     }
 
+    private void docDsSpTheoTh() {
+        ArrayList<SanPham> dsSpTheoTh = new ArrayList<>();
+        if (th != null) {
+            for (int i = 0; i < ds.size(); i++) {
+                if (ds.get(i).getPhanloai().equals(th.getIdth())) {
+                    dsSpTheoTh.add(ds.get(i));
+                }
+            }
+            ds.clear();
+            for (int i = 0; i < dsSpTheoTh.size(); i++) {
+                ds.add(dsSpTheoTh.get(i));
+            }
+            lvSanPhamAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void addEvents() {
+        fabThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        HomePageActivity.this,
+                        AddProductActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+//        lvSanPham.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(
+//                        HomePageActivity.this,
+//                        DetailsProductActivity.class
+//                );
+//                intent.putExtra("chitiet", ds.get(position));
+//                startActivity(intent);
+//            }
+//        });
+
+        lvSanPham.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomePageActivity.this);
+                builder.setIcon(R.drawable.icons_notification);
+                builder.setTitle(getString(R.string.notification));
+                builder.setMessage(getString(R.string.message));
+                builder.setPositiveButton(getString(R.string.agree), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SanPham sp = ds.get(position);
+                        SQLiteDatabase database = openOrCreateDatabase(
+                                DB_NAME,
+                                MODE_PRIVATE,
+                                null
+                        );
+                        int deletedRowCount = database.delete(
+                                "sanpham",
+                                "id=?",
+                                new String[]{sp.getId() + ""}
+                        );
+                        Toast.makeText(
+                                HomePageActivity.this,
+                                getString(R.string.toast_delete),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        docDsSpTuDb();
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
+    }
+
+    private void addControls() {
+        ds = new ArrayList<>();
+        lvSanPham = findViewById(R.id.lvSanPham);
+        fabThem = findViewById(R.id.fabThem);
+    }
+
+    private void docDsSpTuDb() {
+        SQLiteDatabase database = openOrCreateDatabase(
+                DB_NAME,
+                MODE_PRIVATE,
+                null
+        );
+        Cursor cursor = database.rawQuery("Select * From sanpham", null);
+        ds.clear();
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String txtTenSP = cursor.getString(1);
+            int kichthuoc = cursor.getInt(2);
+            int gia = cursor.getInt(3);
+            String txtPhanLoai = cursor.getString(4);
+            byte[] imgHinh = cursor.getBlob(5);
+
+            SanPham sp = new SanPham(id, txtTenSP, txtPhanLoai, imgHinh, String.valueOf(gia), String.valueOf(kichthuoc));
+            ds.add(sp);
+        }
+        lvSanPhamAdapter = new lvAdapter(ds,
+                HomePageActivity.this,
+                R.layout.listviewsp
+        );
+        lvSanPham.setAdapter(lvSanPhamAdapter);
+        lvSanPhamAdapter.notifyDataSetChanged();
+        cursor.close();
+        database.close();
+    }
+
+    private void copyDBFromAsssets() {
+        File dbFile = getDatabasePath(DB_NAME);
+        if (!dbFile.exists()) {
+            try {
+                File dbDir = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+                if (!dbDir.exists()) dbDir.mkdir();
+
+                InputStream is = getAssets().open(DB_NAME);
+                String outputFilePath = getApplicationInfo().dataDir + DB_PATH_SUFFIX + DB_NAME;
+                OutputStream os = new FileOutputStream(outputFilePath);
+                byte[] buffer = new byte[1024];
+                int length = 0;
+                while ((length = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, length);
+                }
+                os.flush();
+                os.close();
+                is.close();
+            } catch (Exception ex) {
+                ex.fillInStackTrace();
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
